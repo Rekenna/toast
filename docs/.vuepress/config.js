9214de3bd3ec2a68d565e6a8489f4974f16d54f2
Ryan McKenna <ryanpatmckenna@gmail.com>
2019-05-20 18:38:24 -0400

    Circle CI

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,27 @@
 module.exports = {
   title: "Toast",
-  description: "Documentation for the Toast WordPress theme.",
-  // Repository Name
-  base: "/toast/"
+  version: "1.2",
+  base: "/toast/",
+  description:
+    "A development focused WordPress starter theme to complete your balanced breakfast.",
+  base: "/horizon/",
+  themeConfig: {
+    logo: "/upload/horizon-logo.png",
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "About", link: "/about.html" },
+      { text: "Getting Started", link: "/getting-started.html" }
+    ],
+    // Assumes GitHub. Can also be a full GitLab url.
+    repo: "rekenna/toast",
+    // Customising the header label
+    // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
+    // repoLabel: "Contribute!",
+    // if your docs are in a specific branch (defaults to 'master'):
+    docsBranch: "master",
+    // defaults to false, set to true to enable
+    editLinks: true,
+    // custom text for edit link. Defaults to "Edit this page"
+    editLinkText: "Help us improve this page!"
+  }
 };
---
+++ docs/getting-started.md
@@ -0,0 +1,6 @@
+---
+---
+
+# Getting Started
+
+**Toast is not a framework**. Toast is a theme loaded up with all of the fixins, designed to make the setup & configuration process easy and painless. _However_, Toast does not do a lot of the actual work for you. It's unbiased in it's configuration outside of it's architecture and lets' you have full control over your WordPress Theme development.
